e0023c5821219eaf5ae1c798b6ff773defb26e8d
Icode4u <88455386+sfx2me@users.noreply.github.com>
2022-05-27 14:03:01 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+import os
+import pystyle
+import time
+from colorama import Fore, Back, Style, init
+from slowprint.slowprint import *
+from os import system
+init(convert=True)
+os.system('cls')
+system("title " + "Super Files Bomb")
+print(Fore.LIGHTRED_EX + """
+ _____ _______   _______   _   _  ___________ _____ 
+|_   _|  ___\ \ / /_   _| | | | ||  ___| ___ \  ___|
+  | | | |__  \ V /  | |   | |_| || |__ | |_/ / |__  
+  | | |  __| /   \  | |   |  _  ||  __||    /|  __| 
+  | | | |___/ /^\ \ | |   | | | || |___| |\ \| |___ 
+  \_/ \____/\/   \/ \_/   \_| |_/\____/\_| \_\____/ """ + Fore.RESET )
+a = input(f"""
+
+
+TEXT ? : """)
+print(Fore.LIGHTRED_EX)
+input("Lightred")
+print(Fore.GREEN)
+input("Green")
+
+c = 1
+d = int(many)
